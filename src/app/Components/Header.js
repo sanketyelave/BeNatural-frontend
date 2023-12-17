becb34bb157d09bd91e408643696079d950e6c25
Sanket <sanketyelave11@gmail.com>
2023-12-17 16:00:32 +0530

    Update README.md

--- src/app/Components/Header.js
+++ src/app/Components/Header.js
@@ -58,12 +58,12 @@ function Header() {
 
     return (
 
-        <header className={`header  ${isMobileMenuOpen ? 'nav-open' : ''}`}>
+        <header className={`header  ${isMobileMenuOpen ? 'nav-open' : ''}`} style={{ zIndex: '300' }}>
             <a href="#">
                 <img className="logo" alt="Be natural logo" src="img/Benatural.png" />
             </a>
 
-            <nav className={`main-nav ${isMobileMenuOpen ? 'open' : ''}`} style={{ zIndex: '2' }}>
+            <nav className={`main-nav ${isMobileMenuOpen ? 'open' : ''}`} style={{ zIndex: '300' }}>
                 <ul className="main-nav-list" style={{ fontFamily: "Rubik" }}>
                     <li><a className="main-nav-link" href="/home" style={{ fontFamily: "Rubik" }} onClick={() => window.location.href = '/home'}>Home <ion-icon name="home-outline"></ion-icon></a></li>
                     <li><a className="main-nav-link" href="/products" onClick={() => window.location.href = '/products'}>Products<ion-icon name="basket-outline"></ion-icon></a></li>
@@ -72,7 +72,7 @@ function Header() {
                         <li><a className="main-nav-link" href="/add" onClick={() => window.location.href = '/products'}>Add to Store<ion-icon name="add"></ion-icon></a></li>
                     }
                     <li><a className="main-nav-link" href="cart" onClick={() => window.location.href = '/cart'}>Cart<ion-icon name="cart-outline"></ion-icon></a></li>
-                    <li><a className="main-nav-link nav-cta" href="/" onClick={() => window.location.href = '/'}>Log out</a></li>
+                    <li><a className="main-nav-link nav-cta" href="/" onClick={() => window.location.href = '/'} style={{ zIndex: '999' }}>Log out</a></li>
                 </ul>
             </nav>
 
